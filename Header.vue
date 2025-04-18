95a94280a3ac3a67493e3248c2b1e12aa3a9260b
Mhmmdneil <141992443+Mhmmdneil@users.noreply.github.com>
2025-04-18 18:13:47 +0700

    Add files via upload

---
+++ Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="conten">
+    <div class="row p-3 text-black">
+      <div class="col-lg-1">
+        <img src="~/assets/img/perjuangan.png" alt="perjuangan">
+      </div>
+
+      <div class="col-lg-11">
+        <h1>Perpus Digital</h1>
+        <address>
+          <p>UNIVERSITAS PERJUANGAN</p>
+          Jl. Pembela Tanah Air (PETA) No. 177, Tasikmalaya
+        </address>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.conten{
+  background-color: rgb(5, 71, 73);
+  font-size: 25px;
+}
+
+.alamat{
+  font-family: jaldi;
+  font-size: 25px;
+}
+
+img {
+  width: 100px;
+}
+/*
+@media (max-width: 912px){
+  img {
+    width: 90px;
+}
+.judul {
+  letter-spacing: 10px;
+  font-size: 50px;
+  font-family: "jockey one";
+  padding-left: 20px;
+}
+.alamat {
+  font-family: "jaldi";
+  padding-left: 20px;
+  }
+}
+@media (max-width: 600px){
+  img{
+    width: 70px;
+}
+.alamat {
+  font-family: "jaldi";
+  padding-left: 20px;
+  }
+} */
+</style>
